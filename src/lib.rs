0cbf0db2048d05fb65f566fa8b358d36270412d2
Frederick Gnodtke <frederick@gnodtke.net>
2016-03-14 20:19:35 +0100

    Added: Can now connect to mumble server

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,23 @@ extern crate protobuf;
 
 mod mumble;
 
-use std::io::prelude::*;
+// use std::io::prelude::*;
 use std::net::TcpStream;
+use std::string::String;
+use protobuf::Message;
+
+fn encode_version(major: u16, minor: u8, patch: u8) -> u32 {
+    return ((major as u32 & 0xFFFF) << 16) |
+        ((minor as u32 & 0xFF) << 8) |
+        (patch as u32 & 0xFF);
+}
 
 pub fn connect(url: &str) {
     let mut stream = TcpStream::connect(url).unwrap();
-    let _ = stream.read(&mut [0; 128]);
+    let mut version = mumble::Version::new();
+
+    let mut proto_stream = protobuf::CodedOutputStream::new(&mut stream);
+    version.set_version(encode_version(1, 2, 7));
+    version.set_release(String::from("Rumble"));
+    let _ = version.write_to_with_cached_sizes(&mut proto_stream);
 }
