64af5c418822a9f11ecf6645e7e35146387e325e
Frederick Gnodtke <frederick@gnodtke.net>
2016-03-21 22:46:01 +0100

    Added: Created methods for protocl operations

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate openssl;
 mod mumble;
 
 use std::string::String;
-use protobuf::Message;
+use protobuf::{Message, CodedOutputStream, CodedInputStream};
 use std::net::TcpStream;
 use openssl::ssl::*;
 use std::sync::Arc;
@@ -26,36 +26,56 @@ fn setup_tls(key: &str, cert: &str) -> Arc<SslContext> {
     return Arc::new(ctx);
 }
 
+fn send_version(output_stream: &mut CodedOutputStream) {
+    let mut version = mumble::Version::new();
+    version.set_version(encode_version(1, 2, 7));
+    version.set_release(String::from("Rumble"));
+    let result = version.write_to_with_cached_sizes(output_stream);
+    println!("Sent Version, Status: {:?}", result);
+}
+
+fn send_auth(output_stream: &mut CodedOutputStream) {
+    let mut authenticate = mumble::Authenticate::new();
+    authenticate.set_username(String::from("RumbleTestbot"));
+    let result = authenticate.write_to_with_cached_sizes(output_stream);
+    println!("Sent Authenticate, Status: {:?}", result);
+}
+
+fn read_version(input_stream: &mut CodedInputStream) {
+    let mut version = mumble::Version::new();
+    let result = version.merge_from(input_stream);
+    println!("Received Version, Status: {:?}, Data: {:?}", result, version);
+}
+
+fn read_crypt(input_stream: &mut CodedInputStream) {
+    let mut crypt = mumble::CryptSetup::new();
+    let result = crypt.merge_from(input_stream);
+    println!("Received Crypt, Status: {:?}, Data: {:?}", result, crypt);
+}
+
+fn read_channelstate(input_stream: &mut CodedInputStream) {
+    let mut channelstate = mumble::ChannelState::new();
+    let result = channelstate.merge_from(input_stream);
+    println!("Received ChannelState, Status: {:?}, Data: {:?}", result, channelstate);
+}
+
+
 pub fn connect(url: &str, key: &str, cert: &str) {
     let tcp = TcpStream::connect(url).unwrap();
     let ctx = setup_tls(key, cert);
-    let mut stream = SslStream::connect(&*ctx, tcp).unwrap();
-    {
-        let mut output_stream = protobuf::CodedOutputStream::new(&mut stream);
-        let mut version = mumble::Version::new();
-        version.set_version(encode_version(1, 2, 7));
-        version.set_release(String::from("Rumble"));
-        let result = version.write_to_with_cached_sizes(&mut output_stream);
-        println!("Sent Version, Status: {:?}", result);
-    }
-    {
-        let mut input_stream = protobuf::CodedInputStream::new(&mut stream);
-        let mut version = mumble::Version::new();
-        let result = version.merge_from(&mut input_stream);
-        println!("Received Version, Status: {:?}, Data: {:?}", result, version);
-    }
-    {
-        let mut output_stream = protobuf::CodedOutputStream::new(&mut stream);
-        let mut authenticate = mumble::Authenticate::new();
-        authenticate.set_username(String::from("RumbleTestbot"));
-        let result = authenticate.write_to_with_cached_sizes(&mut output_stream);
-        println!("Sent Authenticate, Status: {:?}", result);
-    }
-    {
-        let mut input_stream = protobuf::CodedInputStream::new(&mut stream);
-        let mut crypt = mumble::CryptSetup::new();
-        let result = crypt.merge_from(&mut input_stream);
-        println!("Received Crypto, Status: {:?}, Data: {:?}", result, crypt);
-    }
+    let mut stream1 = SslStream::connect(&*ctx, tcp).unwrap();
+    let mut stream2 = stream1.try_clone().unwrap();
+    let mut output_stream = protobuf::CodedOutputStream::new(&mut stream1);
+    let mut input_stream = protobuf::CodedInputStream::new(&mut stream2);
+    // Once the TLS handshake is completed both sides should transmit their version information
+    // using the Version message.
+    send_version(&mut output_stream);
+    // Once the client has sent the version it should follow this with the Authenticate message.
+    // This message may be sent immediately after sending the version message.
+    // The client does not need to wait for the server version message.
+    send_auth(&mut output_stream);
+    read_version(&mut input_stream);
+    // Once the Version packets are exchanged the server will send a CryptSetup packet to the client.
+    read_crypt(&mut input_stream);
 
 }
