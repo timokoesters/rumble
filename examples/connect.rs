64d2da927658c70b0fe23eff1b5041df35eaf736
timokoesters <timo.koesters@gmail.com>
2018-11-23 17:59:07 +0100

    Updated to work with newer openssl versions

--- examples/connect.rs
+++ examples/connect.rs
@@ -4,8 +4,9 @@ use rumble::{connect};
 use std::path::Path;
 
 fn main() {
-    connect("localhost:64738", Path::new("bot.key"), Path::new("bot.crt"));
+    let mut connection = connect("localhost:64738", Path::new("bot.key"), Path::new("bot.crt"));
+    connection.send_version();
     loop {
-
+        connection.read();
     }
 }
